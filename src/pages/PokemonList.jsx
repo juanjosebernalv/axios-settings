4690a9c82af1beaa549193b70663f6f4dce7961c
Juan Jose Bernal Villamarin <juanjosebernalvillamarin@gmail.com>
2024-01-16 16:02:50 -0500

    Created axios initial configuration and settings

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -1,18 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TestApi } from '../api/TestApi';
 
 export const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     const loadPokemons = async () => {
       const result = TestApi.getPokemons();
       console.log(result);
+      const response = await result;
+      console.log('response.data', response.data);
+      setPokemons(response.data);
     };
     loadPokemons();
   }, []);
 
   return (
-    <div>PokemonList</div>
+    <div>
+      <h1>
+
+        PokemonList
+      </h1>
+      <pre>
+        {JSON.stringify(pokemons, null, 2)}
+      </pre>
+    </div>
   );
 };
 
